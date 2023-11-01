5b78cf1710a7279ba8a64c12b084a55307f3c71c
caffrey928 <caffreyacnologialawace@gmail.com>
2023-11-02 05:29:34 +0800

    Add error handling when data not exists

--- contracts/arch/src/lib.rs
+++ contracts/arch/src/lib.rs
@@ -1,16 +1,34 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, symbol_short, Env, Symbol, Vec, vec, Map, I256, map};
+use soroban_sdk::{contract, contractimpl, symbol_short, Env, Symbol, Vec, vec, Map, I256, map, String};
+use soroban_sdk::contracttype;
+// use soroban_sdk::testutils::Address as TraitAddress;
 
 const ID: Symbol = symbol_short!("ID");
 const USER: Symbol = symbol_short!("USER");
 const CLEAR: Symbol = symbol_short!("CLEAR");
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum ReturnMessage {
+    TRANSACTION(Map<Vec<Symbol>, I256>),
+    TRANSACTIONIDS(Vec<Symbol>),
+    TRANSACTIONS(Vec<Map<Vec<Symbol>, I256>>),
+    USERUSAGE(Map<Symbol, I256>),
+    CLEAR(Map<Symbol, I256>),
+    ERROR(String),
+}
 
 #[contract]
 pub struct ArchContract;
 
 #[contractimpl]
 impl ArchContract {
-    pub fn add_transaction(env: Env, id: Symbol, user: Symbol, telecom_pay: Symbol, telecom_receive: Symbol, usage: i32) -> Map<Vec<Symbol>, I256> {
+    pub fn add_transaction(env: Env, id: Symbol, user: Symbol, telecom_pay: Symbol, telecom_receive: Symbol, usage: i32) -> ReturnMessage {
+        let find_transaction: Map<Vec<Symbol>, I256> = env.storage().persistent().get(&id).unwrap_or(Map::new(&env));
+        if find_transaction != Map::new(&env) {
+            return ReturnMessage::ERROR(String::from_slice(&env, "Replicated ID")); // Return from the main function
+        }
+
         let mut transaction_ids: Vec<Symbol> = env.storage().persistent().get(&ID).unwrap_or(Vec::new(&env));
         transaction_ids.push_back(id.clone());
 
@@ -23,23 +41,33 @@ impl ArchContract {
         env.storage().persistent().bump(&ID, 100, 500);
         env.storage().persistent().bump(&id, 100, 500);
 
-        transaction
+        return ReturnMessage::TRANSACTION(transaction);
     }
 
-    pub fn get_transaction_ids(env: Env) -> Vec<Symbol> {
+    pub fn get_transaction_ids(env: Env) -> ReturnMessage {
         let transaction_ids: Vec<Symbol> = env.storage().persistent().get(&ID).unwrap_or(Vec::new(&env));
+        if transaction_ids == Vec::new(&env) {
+            return ReturnMessage::ERROR(String::from_slice(&env, "No Transaction Exist!")); // Return from the main function
+        }
 
-        transaction_ids
+        ReturnMessage::TRANSACTIONIDS(transaction_ids)
     }
 
-    pub fn get_transaction(env: Env, id: Symbol) -> Map<Vec<Symbol>, I256> {
+    pub fn get_transaction(env: Env, id: Symbol) -> ReturnMessage {
         let transaction: Map<Vec<Symbol>, I256> = env.storage().persistent().get(&id).unwrap_or(Map::new(&env));
+        if transaction == Map::new(&env) {
+            return ReturnMessage::ERROR(String::from_slice(&env, "Transaction Not Exist!")); // Return from the main function
+        }
 
-        transaction
+        ReturnMessage::TRANSACTION(transaction)
     }
 
-    pub fn get_all_transactions(env: Env) -> Vec<Map<Vec<Symbol>, I256>> {
+    pub fn get_all_transactions(env: Env) -> ReturnMessage {
         let transaction_ids: Vec<Symbol> = env.storage().persistent().get(&ID).unwrap_or(Vec::new(&env));
+        if transaction_ids == Vec::new(&env) {
+            return ReturnMessage::ERROR(String::from_slice(&env, "No Transaction Exist!")); // Return from the main function
+        }
+
         let mut transactions: Vec<Map<Vec<Symbol>, I256>> = Vec::new(&env);
 
         for id in transaction_ids.iter() {
@@ -47,13 +75,17 @@ impl ArchContract {
             transactions.push_back(transaction);
         }
 
-        transactions
+        ReturnMessage::TRANSACTIONS(transactions)
     }
 
-    pub fn remove_transaction(env: Env, id: Symbol) -> Map<Vec<Symbol>, I256> {
-        let mut transaction_ids: Vec<Symbol> = env.storage().persistent().get(&ID).unwrap_or(Vec::new(&env));
+    pub fn remove_transaction(env: Env, id: Symbol) -> ReturnMessage {
         let transaction: Map<Vec<Symbol>, I256> = env.storage().persistent().get(&id).unwrap_or(Map::new(&env));
+        if transaction == Map::new(&env) {
+            return ReturnMessage::ERROR(String::from_slice(&env, "Transaction Not Exist!")); // Return from the main function
+        }
 
+        let mut transaction_ids: Vec<Symbol> = env.storage().persistent().get(&ID).unwrap_or(Vec::new(&env));
+        
         for (index, transaction_id) in transaction_ids.iter().enumerate() {
             if transaction_id == id {
                 transaction_ids.remove(index as u32);
@@ -65,11 +97,14 @@ impl ArchContract {
         env.storage().persistent().set(&ID, &transaction_ids);
         env.storage().persistent().bump(&ID, 100, 500);
 
-        transaction
+        ReturnMessage::TRANSACTION(transaction)
     }
 
-    pub fn remove_ids(env: Env) -> Vec<Symbol> {
+    pub fn remove_ids(env: Env) -> ReturnMessage {
         let transaction_ids: Vec<Symbol> = env.storage().persistent().get(&ID).unwrap_or(Vec::new(&env));
+        if transaction_ids == Vec::new(&env) {
+            return ReturnMessage::ERROR(String::from_slice(&env, "No Transaction Exist!")); // Return from the main function
+        }
 
         for id in transaction_ids.iter() {
             env.storage().persistent().remove(&id);
@@ -77,67 +112,89 @@ impl ArchContract {
 
         env.storage().persistent().remove(&ID);
 
-        transaction_ids
+        ReturnMessage::TRANSACTIONIDS(transaction_ids)
     }
 
-    pub fn user_usage(env: Env) -> Map<Symbol, I256> {
-        let transactions: Vec<Map<Vec<Symbol>, I256>> = Self::get_all_transactions(env.clone());
-        let mut user_usage_record: Map<Symbol, I256> = env.storage().persistent().get(&USER).unwrap_or(Map::new(&env));
+    pub fn user_usage(env: Env) -> ReturnMessage {
+        let fn_result: ReturnMessage = Self::get_all_transactions(env.clone());
+        match fn_result {
+            ReturnMessage::TRANSACTIONS(transactions) => {
+                let mut user_usage_record: Map<Symbol, I256> = env.storage().persistent().get(&USER).unwrap_or(Map::new(&env));
 
-        for transaction in transactions.iter() {
-            let user: Symbol = transaction.keys().get(0).unwrap_or(Vec::new(&env)).get(0).unwrap_or(symbol_short!(""));
-            let usage: I256 = transaction.values().get(0).unwrap_or(I256::from_i32(&env, 0));
+                for transaction in transactions.iter() {
+                    let user: Symbol = transaction.keys().get(0).unwrap_or(Vec::new(&env)).get(0).unwrap_or(symbol_short!(""));
+                    let usage: I256 = transaction.values().get(0).unwrap_or(I256::from_i32(&env, 0));
 
-            user_usage_record.set(user.clone(), user_usage_record.get(user.clone()).unwrap_or(I256::from_i32(&env, 0)).add(&usage));
+                    user_usage_record.set(user.clone(), user_usage_record.get(user.clone()).unwrap_or(I256::from_i32(&env, 0)).add(&usage));
+                }
+
+                env.storage().persistent().set(&USER, &user_usage_record);
+                env.storage().persistent().bump(&USER, 100, 500);
+
+                ReturnMessage::USERUSAGE(user_usage_record)
+            },
+            _ => ReturnMessage::ERROR(String::from_slice(&env, "No Transactions Exist!")),
+        }
+    }
+
+    pub fn get_user_usage(env: Env) -> ReturnMessage {
+        let user_usage_record: Map<Symbol, I256> = env.storage().persistent().get(&USER).unwrap_or(Map::new(&env));
+        if user_usage_record == Map::new(&env) {
+            return ReturnMessage::ERROR(String::from_slice(&env, "User_Usage Record Not Exist!")); // Return from the main function
         }
 
-        env.storage().persistent().set(&USER, &user_usage_record);
-        env.storage().persistent().bump(&USER, 100, 500);
-
-        user_usage_record
-    }
-
-    pub fn get_user_usage(env: Env) -> Map<Symbol, I256> {
-        let user_usage_record: Map<Symbol, I256> = env.storage().persistent().get(&USER).unwrap_or(Map::new(&env));
-
-        user_usage_record
+        ReturnMessage::USERUSAGE(user_usage_record)
     }
 
     pub fn remove_user_usage(env: Env) {
         env.storage().persistent().remove(&USER);
     }
 
-    pub fn clear(env: Env) -> Map<Symbol, I256> {
-        let transactions: Vec<Map<Vec<Symbol>, I256>> = Self::get_all_transactions(env.clone());
-        let mut clear_record: Map<Symbol, I256> = env.storage().persistent().get(&CLEAR).unwrap_or(Map::new(&env));
+    pub fn clear(env: Env) -> ReturnMessage {
+        let fn_result: ReturnMessage = Self::get_all_transactions(env.clone());
+        match fn_result {
+            ReturnMessage::TRANSACTIONS(transactions) => {
+                let mut clear_record: Map<Symbol, I256> = env.storage().persistent().get(&CLEAR).unwrap_or(Map::new(&env));
 
-        for transaction in transactions.iter() {
-            let telecom_pay: Symbol = transaction.keys().get(0).unwrap_or(Vec::new(&env)).get(1).unwrap_or(symbol_short!(""));
-            let telecom_receive: Symbol = transaction.keys().get(0).unwrap_or(Vec::new(&env)).get(2).unwrap_or(symbol_short!(""));
-            let usage: I256 = transaction.values().get(0).unwrap_or(I256::from_i32(&env, 0));
-            let cost: I256 = usage.mul(&I256::from_i32(&env, 100));
+                for transaction in transactions.iter() {
+                    let telecom_pay: Symbol = transaction.keys().get(0).unwrap_or(Vec::new(&env)).get(1).unwrap_or(symbol_short!(""));
+                    let telecom_receive: Symbol = transaction.keys().get(0).unwrap_or(Vec::new(&env)).get(2).unwrap_or(symbol_short!(""));
+                    let usage: I256 = transaction.values().get(0).unwrap_or(I256::from_i32(&env, 0));
+                    let cost: I256 = usage.mul(&I256::from_i32(&env, 100));
 
-            clear_record.set(telecom_pay.clone(), clear_record.get(telecom_pay.clone()).unwrap_or(I256::from_i32(&env, 0)).sub(&cost));
-            clear_record.set(telecom_receive.clone(), clear_record.get(telecom_receive.clone()).unwrap_or(I256::from_i32(&env, 0)).add(&cost));
+                    clear_record.set(telecom_pay.clone(), clear_record.get(telecom_pay.clone()).unwrap_or(I256::from_i32(&env, 0)).sub(&cost));
+                    clear_record.set(telecom_receive.clone(), clear_record.get(telecom_receive.clone()).unwrap_or(I256::from_i32(&env, 0)).add(&cost));
+                }
+
+                env.storage().persistent().set(&CLEAR, &clear_record);
+                env.storage().persistent().bump(&CLEAR, 100, 500);
+
+                Self::remove_ids(env.clone());
+
+                ReturnMessage::CLEAR(clear_record)
+            }
+            _ => ReturnMessage::ERROR(String::from_slice(&env, "No Transactions Exist!")),
         }
-
-        env.storage().persistent().set(&CLEAR, &clear_record);
-        env.storage().persistent().bump(&CLEAR, 100, 500);
-
-        Self::remove_ids(env.clone());
-
-        clear_record
     }
 
-    pub fn get_clear_record(env: Env) -> Map<Symbol, I256> {
+    pub fn get_clear_record(env: Env) -> ReturnMessage {
         let clear_record: Map<Symbol, I256> = env.storage().persistent().get(&CLEAR).unwrap_or(Map::new(&env));
+        if clear_record == Map::new(&env) {
+            return ReturnMessage::ERROR(String::from_slice(&env, "Clear Record Not Exist!")); // Return from the main function
+        }
 
-        clear_record
+        ReturnMessage::CLEAR(clear_record)
     }
 
     pub fn remove_clear_record(env: Env) {
         env.storage().persistent().remove(&CLEAR);
     }
+
+    // pub fn random_address(address: Address) -> Address {
+    //     // let address: Address = <Address as TraitAddress>::random(&env);
+
+    //     address
+    // }
 }
 
 #[cfg(test)]
--- contracts/arch/src/test.rs
+++ contracts/arch/src/test.rs
@@ -1,27 +1,34 @@
-use crate::{ArchContract, ArchContractClient};
-use soroban_sdk::{symbol_short, vec, map, Env, I256};
+use crate::{ArchContract, ArchContractClient, ReturnMessage};
+use soroban_sdk::{symbol_short, vec, map, Env, I256, Map, Symbol, Vec};
+// use soroban_sdk::testutils::Address as TraitAddress;
 
 #[test]
 fn arch() {
     let env = Env::default();
     let contract_id = env.register_contract(None, ArchContract);
     let client = ArchContractClient::new(&env, &contract_id);
+    // let address: Address = Address::random(&env);
 
-    let transaction_1 = client.add_transaction(&symbol_short!("test_1"), &symbol_short!("Alice"), &symbol_short!("Telecom1"), &symbol_short!("Telecom2"), &1000);
-    let transaction_2 = client.add_transaction(&symbol_short!("test_2"), &symbol_short!("Bob"), &symbol_short!("Telecom2"), &symbol_short!("Telecom1"), &2000);
-    let transaction_3 = client.add_transaction(&symbol_short!("test_3"), &symbol_short!("Sam"), &symbol_short!("Telecom3"), &symbol_short!("Telecom2"), &3000);
+    let transaction_1: ReturnMessage = client.add_transaction(&symbol_short!("test_1"), &symbol_short!("Alice"), &symbol_short!("Telecom1"), &symbol_short!("Telecom2"), &1000);
+    let transaction_2: ReturnMessage = client.add_transaction(&symbol_short!("test_2"), &symbol_short!("Bob"), &symbol_short!("Telecom2"), &symbol_short!("Telecom1"), &2000);
+    let transaction_3: ReturnMessage = client.add_transaction(&symbol_short!("test_3"), &symbol_short!("Sam"), &symbol_short!("Telecom3"), &symbol_short!("Telecom2"), &3000);
 
-    assert_eq!(client.get_transaction_ids(), vec![&env, symbol_short!("test_1"), symbol_short!("test_2"), symbol_short!("test_3")]);
-    assert_eq!(client.get_all_transactions(), vec![&env, transaction_1.clone(), transaction_2.clone(), transaction_3.clone()]);
+    let transaction_1_clone: Map<Vec<Symbol>, I256> = map![&env, (vec![&env, symbol_short!("Alice"), symbol_short!("Telecom1"), symbol_short!("Telecom2")], I256::from_i32(&env, 1000))];
+    let transaction_2_clone: Map<Vec<Symbol>, I256> = map![&env, (vec![&env, symbol_short!("Bob"), symbol_short!("Telecom2"), symbol_short!("Telecom1")], I256::from_i32(&env, 2000))];
+    let transaction_3_clone: Map<Vec<Symbol>, I256> = map![&env, (vec![&env, symbol_short!("Sam"), symbol_short!("Telecom3"), symbol_short!("Telecom2")], I256::from_i32(&env, 3000))];
+
+    assert_eq!(transaction_1, ReturnMessage::TRANSACTION(map![&env, (vec![&env, symbol_short!("Alice"), symbol_short!("Telecom1"), symbol_short!("Telecom2")], I256::from_i32(&env, 1000))]));
+    assert_eq!(client.get_transaction_ids(), ReturnMessage::TRANSACTIONIDS(vec![&env, symbol_short!("test_1"), symbol_short!("test_2"), symbol_short!("test_3")]));
+    assert_eq!(client.get_all_transactions(), ReturnMessage::TRANSACTIONS(vec![&env, transaction_1_clone, transaction_2_clone, transaction_3_clone]));
     assert_eq!(client.get_transaction(&symbol_short!("test_1")), transaction_1);
     assert_eq!(client.get_transaction(&symbol_short!("test_2")), transaction_2);
     assert_eq!(client.get_transaction(&symbol_short!("test_3")), transaction_3);
 
     client.user_usage();
-    assert_eq!(client.get_user_usage(), map![&env, (symbol_short!("Alice"), I256::from_i32(&env, 1000)), (symbol_short!("Bob"), I256::from_i32(&env, 2000)), (symbol_short!("Sam"), I256::from_i32(&env, 3000))]);
+    assert_eq!(client.get_user_usage(), ReturnMessage::USERUSAGE(map![&env, (symbol_short!("Alice"), I256::from_i32(&env, 1000)), (symbol_short!("Bob"), I256::from_i32(&env, 2000)), (symbol_short!("Sam"), I256::from_i32(&env, 3000))]));
     
     client.clear();
-    assert_eq!(client.get_clear_record(), map![&env, (symbol_short!("Telecom1"), I256::from_i32(&env, 100000)), (symbol_short!("Telecom2"), I256::from_i32(&env, 200000)), (symbol_short!("Telecom3"), I256::from_i32(&env, -300000))]);
+    assert_eq!(client.get_clear_record(), ReturnMessage::CLEAR(map![&env, (symbol_short!("Telecom1"), I256::from_i32(&env, 100000)), (symbol_short!("Telecom2"), I256::from_i32(&env, 200000)), (symbol_short!("Telecom3"), I256::from_i32(&env, -300000))]));
 
     // client.remove_transaction(&symbol_short!("test_3"));
     // assert_eq!(client.get_transaction_ids(), vec![&env, symbol_short!("test_1"), symbol_short!("test_2")]);
@@ -29,4 +36,7 @@ fn arch() {
     // client.remove_ids();
     // assert_eq!(client.get_transaction_ids(), vec![&env]);
     // assert_eq!(client.get_transaction(&symbol_short!("test_1")), vec![&env]);
+
+    // client.random_address(&address);
+    // assert_eq!(client.random_address(&address), address);
 }
